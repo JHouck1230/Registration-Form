152b0a2af1d8e17c8983c73dcab47cc106f55336
Josh Houck <Josh123090@gmail.com>
2016-03-15 12:54:12 -0700

    changed form validity conditioning

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,78 +3,82 @@
 var app = angular.module('ccApp');
 
 app.controller('ccCtrl', function($scope, CCService) {
-	// $scope.submitUserForm = function(forInvalid) {
-	// 	if($scope.userFrom.validCC && $scope.userForm.validPass) {
-	// 		return true;
-	// 	} else {
-	// 		return false;
-	// 	}
-	// }
 
-	$scope.checkExpDate = function(expDate) {
-		if(Date.now() < expDate) {
-			return console.log(true);
+	$scope.confirmPass = function() {
+		var password = $scope.user.password;
+		var conPass = $scope.user.conPass;
+		if(password === conPass) {
+			console.log('pass match check: ', password === conPass);
+			return false
 		} else {
-			return console.log(false);
+			console.log('pass match check: ', password === conPass);
+			return true;
 		}
 	}
 
-	$scope.checkCC = function(ccNum) {
+	$scope.checkExpDate = function(expDate) {
+		if(Date.now() < expDate) {
+			return true
+		} else {
+			return false;
+		}
+	}
+
+	$scope.checkCC = function() {
+		var ccNum = $scope.user.ccNum;
+		console.log('ccNum: ', ccNum);
+		if(!ccNum) return;
 		if(ccNum.match(/\D/)) return false;
 		var result = ccNum.split('').reverse().reduce((a, b, i) => {
 			if(i % 2 !== 0) b *= 2;
 			if(b > 9) b = b.toString().split('').reduce((c, d) => parseInt(c) + parseInt(d));
 			return a + parseInt(b);
 		}, 0) * 9;
-		$scope.userForm.validCC = (result % 10 === 0);
-		console.log($scope.userForm.validCC);
-	}
-		// console.log(ccNum.match(/(34|37)/));
-		// switch (ccNum) {
-		// 	case ccNum.match(/(34|37)/):
-		// 		$scope.amex = true;
-		// 		console.log('amex: ', amex);
-		// 		break;
-		// case ccNum.match(/(622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5])|64[4-9]|(65)|6011)/):
-		// 		$scope.disc = true;
-		// 		console.log('disc: ', disc);
-		// 		break;
-		// case ccNum.match(/(5[0-5])/):
-		// 		$scope.mstrcard = true;
-		// 		console.log('mstrcard: ', mstrcard);
-		// 		break;
-		// case ccNum.match(/4026|417500|4405|4508|4844|4913|4917|4/):
-		// 		$scope.amex = true;
-		// 		console.log('amex: ', amex);
-		// 		break;
-		// default:
-		// 		return false;
-		// 		break;
+		if(result % 10 === 0)	{
+			console.log('luhn check: ', result % 10 === 0);
+			return false
+		} else {
+			console.log('luhn check: ', result % 10 === 0);
+			return true;
+		}
+
+		// if(ccNum) {
+		// 	switch (ccNum) {
+		// 		case ccNum.match(/(34|37)/):
+		// 			$scope.amex = true;
+		// 			console.log('amex: ', amex);
+		// 			break;
+		// 		case ccNum.match(/(622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5])|64[4-9]|(65)|6011)/):
+		// 			$scope.disc = true;
+		// 			console.log('disc: ', disc);
+		// 			break;
+		// 		case ccNum.match(/(5[0-5])/):
+		// 			$scope.mstrcard = true;
+		// 			console.log('mstrcard: ', mstrcard);
+		// 			break;
+		// 		case ccNum.match(/4026|417500|4405|4508|4844|4913|4917|4/):
+		// 			$scope.amex = true;
+		// 			console.log('amex: ', amex);
+		// 			break;
+		// 		default:
+		// 			return false;
+		// 			break;
+		// 	}
 		// }
+	}
 
 	$scope.passRegex = /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-	// $scope.checkPassword = function(password) {
-	// 	if(password.match(/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,}/)) {
- //  		$scope.userForm.validPass = true;
- //  	} else {
- //  		$scope.userForm.validPass = false;
- //  	}
-	// }
 
-	// $scope.confirmPass = function(password, conPass) {
-	// 	if(password && conPass) {
-	// 		return password === conPass;
-	// 	} else {
-	// 		return false;
-	// 	}
-	// }
+	$scope.submitUserForm = function(formInvalid) {
+		if(formInvalid) {
 
-	$scope.submitInfo = function() {
-		CCService.saveInfo($scope.user)
-		.then(function(res) {
-			return;
-		}, function(err) {
-			console.error(err);
-		})
+		} else {
+			CCService.saveInfo($scope.user)
+			.then(function(res) {
+				return;
+			}, function(err) {
+				console.error(err);
+			})
+		}
 	}
 });

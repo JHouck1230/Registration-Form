fd2638c4e72c30687711fb15f2f85b1a4155632f
Josh Houck <Josh123090@gmail.com>
2016-03-15 22:51:25 -0700

    minor changes

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -8,25 +8,26 @@ app.controller('ccCtrl', function($scope, CCService) {
 		var password = $scope.user.password;
 		var conPass = $scope.user.conPass;
 		if(password === conPass) {
-			console.log('pass match check: ', password === conPass);
+			$scope.userForm.conPass.notMatch = false;
 			return false
 		} else {
-			console.log('pass match check: ', password === conPass);
+			$scope.userForm.conPass.notMatch = true;
 			return true;
 		}
 	}
 
 	$scope.checkExpDate = function(expDate) {
 		if(Date.now() < expDate) {
-			return true
-		} else {
+			$scope.userForm.expDate.invalidDate = false;
 			return false;
+		} else {
+			$scope.userForm.expDate.invalidDate = true;
+			return true;
 		}
 	}
 
 	$scope.checkCC = function() {
 		var ccNum = $scope.user.ccNum;
-		console.log('ccNum: ', ccNum);
 		if(!ccNum) return;
 		if(ccNum.match(/\D/)) return false;
 		var result = ccNum.split('').reverse().reduce((a, b, i) => {
@@ -35,36 +36,12 @@ app.controller('ccCtrl', function($scope, CCService) {
 			return a + parseInt(b);
 		}, 0) * 9;
 		if(result % 10 === 0)	{
-			console.log('luhn check: ', result % 10 === 0);
+			$scope.userForm.ccNum.luhnError = false;
 			return false
 		} else {
-			console.log('luhn check: ', result % 10 === 0);
+			$scope.userForm.ccNum.luhnError = true;
 			return true;
 		}
-
-		// if(ccNum) {
-		// 	switch (ccNum) {
-		// 		case ccNum.match(/(34|37)/):
-		// 			$scope.amex = true;
-		// 			console.log('amex: ', amex);
-		// 			break;
-		// 		case ccNum.match(/(622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5])|64[4-9]|(65)|6011)/):
-		// 			$scope.disc = true;
-		// 			console.log('disc: ', disc);
-		// 			break;
-		// 		case ccNum.match(/(5[0-5])/):
-		// 			$scope.mstrcard = true;
-		// 			console.log('mstrcard: ', mstrcard);
-		// 			break;
-		// 		case ccNum.match(/4026|417500|4405|4508|4844|4913|4917|4/):
-		// 			$scope.amex = true;
-		// 			console.log('amex: ', amex);
-		// 			break;
-		// 		default:
-		// 			return false;
-		// 			break;
-		// 	}
-		// }
 	}
 
 	$scope.passRegex = /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,}/;
